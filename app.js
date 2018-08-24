276d65093e8921a4b0811319d6001a266224fa40
DanHodges <dhdgs690@gmail.com>
2018-08-24 14:54:34 -0500

    test passing except for strict equality

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ EmptyList.prototype.length = function() {
   return 0;
 };
 EmptyList.prototype.push = function(x) {
-  return new ListNode(x, this);
+  return new ListNode(x);
 };
 EmptyList.prototype.remove = function(x) {
   return new EmptyList();
@@ -35,48 +35,68 @@ ListNode.prototype.isEmpty = function() {
 
 ListNode.prototype.toString = function() {
   const toString = (node, string) => {
-    if (!node) {
+    if (!node || !node.value) {
       return `${string})`;
     }
 
-    return toString(node.next, `${string}${node.value}${node.next ? " " : ""}`);
+    return toString(
+      node.next,
+      `${string}${node.value}${node.next && node.next.value ? " " : ""}`,
+    );
   };
 
   return toString(this, "(");
 };
 
 ListNode.prototype.head = function() {
-  return this;
+  const head = node => {
+    if (!node.next) {
+      return node;
+    }
+
+    return head(node.next);
+  };
+  return head(this);
 };
+
 ListNode.prototype.tail = function() {
-  return this.next;
+  const tail = node => {
+    if (!node.next) {
+      return node;
+    }
+    if (!node.next.next) {
+      return new ListNode(node.value);
+    }
+    return new ListNode(node.value, tail(node.next));
+  };
+
+  return tail(this);
 };
+
 ListNode.prototype.length = function() {
   const length = (node, count) => {
     return !node.next ? count : length(node.next, count + 1);
-  }
+  };
 
   return length(this, 1);
 };
+
 ListNode.prototype.push = function(x) {
-  const push = node => {
-    if (!node) {
-      return new ListNode(x);
-    }
-    return new ListNode(node.value, push(node.next));
-  };
-  return push(this);
+  return new ListNode(x, this);
 };
 
 ListNode.prototype.remove = function(value) {
   const remove = node => {
-    if (node.value === value && node.next && node.next.next) {
-      return new ListNode(node.next.value, node.next.next)
-    }
     if (node.value === value) {
-      return null;
+      if (node.next && node.next.value) {
+        return remove(new ListNode(node.next.value, node.next.next));
+      }
+      return new EmptyList();
     }
-    return new ListNode(node.value, remove(node.next));
+    return new ListNode(
+      node.value,
+      node && node.next ? remove(node.next) : null,
+    );
   };
 
   return remove(this);
@@ -84,12 +104,14 @@ ListNode.prototype.remove = function(value) {
 
 ListNode.prototype.append = function(xs) {
   const append = node => {
-    if (!node) {
-      return xs;
+    if (!node.next) {
+      return this.push(node.value);
     }
+
     return new ListNode(node.value, append(node.next));
   };
-  return append(this);
+
+  return append(xs);
 };
 
 module.exports = {
--- app.test.js
+++ app.test.js
@@ -50,20 +50,30 @@ describe("list node tests", function() {
 
   it("has a head", () => {
     expect(foo.head()).toEqual(new ListNode("foo"));
-  });
-
-  it("can push items", () => {
-    expect(fooBarBaz).toEqual(new ListNode("foo").push("bar").push("baz"));
+    expect(fooBarBaz.head()).toEqual(new ListNode("foo"));
   });
 
   it("converts to string", () => {
-    expect(fooBarBaz.toString()).toEqual("(foo bar baz)");
+    expect(fooBarBaz.toString()).toEqual("(baz bar foo)");
+  });
+
+  it("can push items", () => {
+    expect(fooBarBaz.toString()).toEqual("(baz bar foo)");
   });
 
   it("has a tail", () => {
     expect(fooBarBaz.tail().toString()).toEqual(
-      new ListNode("bar", new ListNode("baz")).toString(),
+      new EmptyList()
+        .push("bar")
+        .push("baz")
+        .toString(),
     );
+    expect(
+      new EmptyList()
+        .push("foo")
+        .tail()
+        .toString(),
+    ).toEqual("(foo)");
   });
 
   it("has a length", () => {
@@ -71,12 +81,58 @@ describe("list node tests", function() {
     expect(fooBarBaz.length()).toEqual(3);
   });
 
-  it("can append items", () => {
-    expect(fooBarBaz.append(foo).toString()).toEqual("(foo bar baz foo)");
+  it("removes items", () => {
+    expect(foo.remove("foo").toString()).toEqual("()");
+    expect(fooBarBaz.remove("foo").toString()).toEqual("(baz bar)");
+    expect(fooBarBaz.remove("bar").toString()).toEqual("(baz foo)");
+    expect(
+      fooBarBaz
+        .push("foo")
+        .remove("foo")
+        .toString(),
+    ).toEqual("(baz bar)");
   });
 
-  // it("can remove items", () => {
-  //   expect(emptyList.remove("foo")).toEqual(new EmptyList());
-  // });
+  it("can append items", () => {
+    expect(fooBarBaz.append(foo.push("qux")).toString()).toEqual(
+      "(qux foo baz bar foo)",
+    );
+  });
+});
 
+describe("codewars spec", () => {
+  let mt, l1, l2, l3, l4;
+
+  beforeEach(() => {
+    mt = new EmptyList();
+    l1 = mt
+      .push("c")
+      .push("b")
+      .push("a");
+    l2 = l1.append(l1);
+    l3 = l1.remove("b");
+    l4 = l2.remove("b");
+  });
+
+  it("Shared structure", () => {
+    expect(l2.toString()).toEqual(
+      new EmptyList()
+        .push("c")
+        .push("b")
+        .push("a")
+        .append(
+          new EmptyList()
+            .push("c")
+            .push("b")
+            .push("a"),
+        )
+        .toString(),
+    );
+    expect(l2.toString()).toEqual("(a b c a b c)");
+    expect(l2.tail().tail().tail().toString()).toEqual(l1.toString());
+    expect(l2.tail().tail().tail()).toEqual(l1);
+    expect(l2.tail().tail().tail()).toBe(l1);
+    // expect(l2 === l1).toBeFalsy();
+    // expect(l3.tail()).toEqual(l1.tail().tail());
+  });
 });
